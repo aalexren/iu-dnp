5a9fda725e72fe4dc0e40d78e236a176ab177409
Rodion <chernitca@ya.ru>
2022-09-10 23:28:33 +0300

    [FIX] -> server

--- week02/server.py
+++ week02/server.py
@@ -7,18 +7,21 @@ import sys, time
 
 def worker(queue):
     while True:
-        client_sock, addr = queue.get()
-        with client_sock:
-            print(f'{addr} client is connected')
-            data = client_sock.recv(1024)
-            numbers = list(map(int, data.decode().split('\n')))
-            ans = []
-            for number in numbers:
-                res = is_prime(number)
-                res = f'{number} is ' + 'not ' * (not res) + 'prime'
-                ans.append(res)
-            client_sock.sendall('\n'.join(ans).encode())
-            print(f'{addr} is disconnected')
+        try:
+            client_sock, addr = queue.get()
+            with client_sock:
+                print(f'{addr} client is connected')
+                data = client_sock.recv(1024)
+                numbers = list(map(int, data.decode().split('\n')))
+                ans = []
+                for number in numbers:
+                    res = is_prime(number)
+                    res = f'{number} is ' + 'not ' * (not res) + 'prime'
+                    ans.append(res)
+                client_sock.sendall('\n'.join(ans).encode())
+                print(f'{addr} is disconnected')
+        except Exception as e:
+            pass
             
 
 def is_prime(n: int) -> bool:
@@ -37,7 +40,7 @@ def main():
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind(('', PORT))
-        sock.listen(5) # size of the queue to connect, refuse new if maximum is reached
+        sock.listen(10) # size of the queue to connect, refuse new if maximum is reached
 
         queue = Queue(10)
 
@@ -53,6 +56,8 @@ def main():
                 print('Shutting down')
                 print('Done')
                 exit()
+            except Exception:
+                pass
 
 
 if __name__ == '__main__':
