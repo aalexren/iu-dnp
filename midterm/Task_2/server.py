dc18df696ad0215b08e673ac1c979bb8b174a874
aalexren <chernicaar@gmail.com>
2022-11-04 00:11:01 +0300

    gracefully terminate threads; need fix

--- midterm/Task_2/server.py
+++ midterm/Task_2/server.py
@@ -2,10 +2,9 @@ import socket
 import threading
 import sys
 
-import time
-
 from multiprocessing import Queue
 from threading import Lock
+from queue import Empty
 
 HOST = ""
 PORT = int(sys.argv[1])
@@ -15,6 +14,11 @@ NAMES: dict[str, tuple] = {}
 CONNECTIONS_POOL = []
 
 lock = Lock()
+
+# trying gracefully exit thread
+# https://superfastpython.com/stop-a-thread-in-python/
+# https://docs.python.org/2/library/threading.html#threading.Event
+event = threading.Event()
 
 def goodbye():
     for name in NAMES.keys():
@@ -26,7 +30,8 @@ def notify_all(names: bytes):
         sock, addr = NAMES[name]
         sock.sendall(names)
 
-def worker_thread(queue):
+def worker_thread(event: threading.Event, queue: Queue):
+    
     client_sock, addr = queue.get()
     name = ''
 
@@ -34,26 +39,26 @@ def worker_thread(queue):
     data = data.decode()
     name = data
     try:
-        if data not in NAMES:
-            lock.acquire()
-            NAMES[data] = (client_sock, addr)
-            lock.release()
-            names = ' '.join(sorted(NAMES.keys()))
-            notify_all(names.encode())
+        if data not in NAMES and not event.is_set():
+            if lock.acquire(timeout=1):
+                NAMES[data] = (client_sock, addr)
+                lock.release()
+                names = ' '.join(sorted(NAMES.keys()))
+                notify_all(names.encode())
     except KeyboardInterrupt:
         return None
-    except:
+    except Exception:
         return None
     
     try:
         request = client_sock.recv(1024)
-        if not request:
-            lock.acquire()
-            del NAMES[name]
-            lock.release()
-            names = ' '.join(sorted(NAMES.keys()))
-            notify_all(names.encode())
-    except:
+        if not request and not event.is_set():
+            if lock.acquire(timeout=1):
+                del NAMES[name]
+                lock.release()
+                names = ' '.join(sorted(NAMES.keys()))
+                notify_all(names.encode())
+    except Exception:
         return None
 
     client_sock.close()
@@ -66,18 +71,28 @@ def main():
 
         queue = Queue(10)
 
-        workers = [threading.Thread(
-            target=worker_thread, args=(queue,), daemon=False
-        ) for _ in range(10)]
-        for worker in workers:
-            worker.start()
+        # FIXME
+        # workers = [threading.Thread(
+        #     target=worker_thread, args=(event, queue,), daemon=False
+        # ) for _ in range(10)]
+        # for worker in workers:
+        #     worker.start()
+        workers: list[threading.Thread] = []
 
         while True:
             try:
                 client_sock, addr = sock.accept()
                 queue.put((client_sock, addr), True)
+                workers.append(threading.Thread(
+                    target=worker_thread, args=(event, queue,), daemon=False
+                    )
+                )
+                workers[-1].start()
             except KeyboardInterrupt:
                 goodbye()
+                event.set()
+                for worker in workers:
+                    worker.join()
                 exit()
 
 if __name__ == "__main__":
